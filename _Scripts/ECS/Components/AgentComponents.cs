9ad2268c89ae0c0d80ae0291c5df5106d203ccee
KalmarAkos <kalmar.akos2@gmail.com>
2025-09-06 21:13:54 +0200

    Kisebb javítások

--- _Scripts/ECS/Components/AgentComponents.cs
+++ _Scripts/ECS/Components/AgentComponents.cs
@@ -1,82 +1,79 @@
+// ---------------------------------------------
+// FILE: Scripts/ECS/Components/AgentComponents.cs
+// ---------------------------------------------
 using Unity.Entities;
 using Unity.Mathematics;
-public struct NPCTag : IComponentData { }
 
-
-public struct MoveSpeed : IComponentData { public float Value; }
-public struct TurnSpeed : IComponentData { public float Value; }
-
-
-public struct Steering : IComponentData
+namespace DOTSGame.Components
 {
-    public float3 DesiredVelocity;
-    public float3 LastVelocity;
-    public float MaxSpeed;
-}
+    public struct PlayerTag : IComponentData { }
+    public struct NPCTag : IComponentData { }
 
+    public struct MoveSpeed : IComponentData { public float Value; }
+    public struct TurnSpeed : IComponentData { public float Value; }
 
-public struct AvoidanceSettings : IComponentData
-{
-    public float LookAhead;
-    public float AvoidWeight;
-    public float NeighborRadius;
-    public float SeparationWeight;
-    public float SeekWeight;
-    public float MaxForce;
-}
+    public struct Steering : IComponentData
+    {
+        public float3 DesiredVelocity;
+        public float3 LastVelocity;
+        public float MaxSpeed;
+    }
 
+    public struct AvoidanceSettings : IComponentData
+    {
+        public float LookAhead;
+        public float AvoidWeight;
+        public float NeighborRadius;
+        public float SeparationWeight;
+        public float SeekWeight;
+        public float MaxForce;
+    }
 
-public struct AgentTarget : IComponentData
-{
-    public float3 Position;
-    public float Radius;
-    public float RepathCooldown;
-    public float RepathTimer;
-    public uint Seed;
-}
+    public struct AgentTarget : IComponentData
+    {
+        public float3 Position;
+        public float Radius;
+        public float RepathCooldown;
+        public float RepathTimer;
+        public uint Seed;
+    }
 
+    public struct Grounding : IComponentData
+    {
+        public float RayLength;
+    }
 
-public struct Grounding : IComponentData
-{
-    public float RayLength;
-}
+    public struct JumpData : IComponentData
+    {
+        public float Gravity;
+        public float JumpImpulse;
+        public float VerticalSpeed;
+        public byte IsGrounded;
+    }
 
+    public struct WorldBounds : IComponentData
+    {
+        public float2 Size;   // pl. (1000,1000)
+        public float BaseY;  // alapszint (pl. Terrain baseline)
+    }
 
-public struct JumpData : IComponentData
-{
-    public float Gravity;
-    public float JumpImpulse;
-    public float VerticalSpeed;
-    public byte IsGrounded;
-}
+    public struct InputSingleton : IComponentData
+    {
+        public float2 Move;  // x=Horizontal (A/D), y=Vertical (W/S)
+        public byte Jump;  // egyszeri leütés
+    }
 
+    public struct NPCSpawner : IComponentData
+    {
+        public Entity Prefab;
+        public int Count;
+        public float2 AreaSize;
+        public float SpawnY;
+        public byte Done; // 0 = még nem spawnolt
+    }
 
-public struct WorldBounds : IComponentData
-{
-    public float2 Size; // pl. (1000,1000)
-    public float BaseY; // alapszint (pl. Terrain baseline)
-}
-
-
-public struct InputSingleton : IComponentData
-{
-    public float2 Move; // x=Horizontal (A/D), y=Vertical (W/S)
-    public byte Jump; // egyszeri leütés
-}
-
-
-public struct NPCSpawner : IComponentData
-{
-    public Entity Prefab;
-    public int Count;
-    public float2 AreaSize;
-    public float SpawnY;
-    public byte Done; // 0 = még nem spawnolt
-}
-
-
-public struct AgentRegistry : IComponentData
-{
-    public int Count; // összes NPC
-}
+    public struct AgentRegistry : IComponentData
+    {
+        public int Count; // összes NPC
+    }
 }
